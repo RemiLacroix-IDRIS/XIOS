52d670f2b605e5b715795a67e02c12856ef5ff00
yushan <yushan@d613d254-d18a-4304-b296-5125d4c4d670>
2017-05-15 13:28:14 +0000

    creating a new branch for merge
    git-svn-id: svn+ssh://forge.ipsl.jussieu.fr/ipsl/forge/projets/ioserver/svn/XIOS@1131 d613d254-d18a-4304-b296-5125d4c4d670

---
+++ dev/branch_yushan_merged/src/filter/file_writer_filter.cpp
@@ -0,0 +1,40 @@
+#include "file_writer_filter.hpp"
+#include "exception.hpp"
+#include "field.hpp"
+#include "utils.hpp"
+
+namespace xios
+{
+  CFileWriterFilter::CFileWriterFilter(CGarbageCollector& gc, CField* field)
+    : CInputPin(gc, 1)
+    , field(field)
+  {
+    if (!field)
+      ERROR("CFileWriterFilter::CFileWriterFilter(CField* field)",
+            "The field cannot be null.");
+  }
+
+  void CFileWriterFilter::onInputReady(std::vector<CDataPacketPtr> data)
+  {
+    bool ignoreMissingValue = (!field->detect_missing_value.isEmpty() && 
+                               !field->default_value.isEmpty() && 
+                               field->detect_missing_value == true);
+    if (ignoreMissingValue)
+    {
+      double missingValue = field->default_value;
+      size_t nbData = data[0]->data.numElements();
+      for (size_t idx = 0; idx < nbData; ++idx)
+      {
+        if (NumTraits<double>::isnan(data[0]->data(idx)))
+          data[0]->data(idx) = missingValue;
+      }
+    }    
+
+    field->sendUpdateData(data[0]->data);
+  }
+
+  bool CFileWriterFilter::isDataExpected(const CDate& date) const
+  {
+    return true;
+  }
+} // namespace xios
