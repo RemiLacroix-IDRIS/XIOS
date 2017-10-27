RemiLacroix-IDRIS/XIOS

commit 287a0645be4403b0388c495f3ac11c0535499003

Improvements for the secondary server: each grid is only sent to secondary-server pools that need it.
git-svn-id: svn+ssh://forge.ipsl.jussieu.fr/ipsl/forge/projets/ioserver/svn/XIOS@1294 d613d254-d18a-4304-b296-5125d4c4d670

dev/XIOS_DEV_CMIP6/src/node/field.cpp
@@ -950,9 +950,12 @@ namespace xios{
      if (hasWriterServer)
      {
         if (!instantDataFilter)
+//          instantDataFilter = clientSourceFilter = boost::shared_ptr<CSourceFilter>(new CSourceFilter(gc, grid, false, NoneDu, false,
+//                                                                                                      detectMissingValues, defaultValue));
           instantDataFilter = clientSourceFilter = boost::shared_ptr<CSourceFilter>(new CSourceFilter(gc, grid,true));
 
-             // If the field data is to be read by the client or/and written to a file
+
+       // If the field data is to be read by the client or/and written to a file
        if (enableOutput && !storeFilter && !fileWriterFilter)
        {
          if (file && (file->mode.isEmpty() || file->mode == CFile::mode_attr::write))
@@ -965,6 +968,8 @@ namespace xios{
      else if (hasIntermediateServer)
      {
        if (!instantDataFilter)
+//         instantDataFilter = clientSourceFilter = boost::shared_ptr<CSourceFilter>(new CSourceFilter(gc, grid, false, NoneDu, false,
+//                                                                                                     detectMissingValues, defaultValue));
          instantDataFilter = clientSourceFilter = boost::shared_ptr<CSourceFilter>(new CSourceFilter(gc, grid, true));
 
              // If the field data is to be read by the client or/and written to a file
@@ -1499,6 +1504,17 @@ namespace xios{
    CVariableGroup* CField::addVariableGroup(const string& id)
    {
      return vVariableGroup->createChildGroup(id);
+   }
+
+   void CField::setContextClient(CContextClient* contextClient)
+   {
+     client = contextClient;
+     grid->setContextClient(contextClient);
+   }
+
+   CContextClient* CField::getContextClient()
+   {
+     return client;
    }
 
    void CField::sendAddAllVariables(CContextClient* client)

dev/XIOS_DEV_CMIP6/src/node/field.hpp
@@ -102,6 +102,9 @@ namespace xios {
          std::map<int, StdSize> getGridAttributesBufferSize(CContextClient* client);
          std::map<int, StdSize> getGridDataBufferSize(CContextClient* client); // Grid data buffer size for each connection of contextclient
 
+         void setContextClient(CContextClient* newContextClient);
+         CContextClient* getContextClient();
+
        public:
          bool isActive(bool atCurrentTimestep = false) const;
          bool hasOutputFile;
@@ -235,6 +238,8 @@ namespace xios {
          DECLARE_REF_FUNC(Field,field)
 
       private:
+         CContextClient* client;
+
          bool areAllReferenceSolved;
          bool isReferenceSolved;
          bool isReferenceSolvedAndTransformed;

dev/XIOS_DEV_CMIP6/src/node/file.cpp
@@ -943,6 +943,11 @@ namespace xios {
    void CFile::setContextClient(CContextClient* newContextClient)
    {
      client = newContextClient;
+     size_t size = this->enabledFields.size();
+     for (size_t i = 0; i < size; ++i)
+     {
+       this->enabledFields[i]->setContextClient(newContextClient);
+     }
    }
 
    CContextClient* CFile::getContextClient()

dev/XIOS_DEV_CMIP6/src/node/grid.cpp
@@ -35,6 +35,7 @@ namespace xios {
       , axisPositionInGrid_(), hasDomainAxisBaseRef_(false)
       , gridSrc_(), hasTransform_(false), isGenerated_(false), order_(), globalIndexOnServer_()
       , computedWrittenIndex_(false)
+      , clients()
    {
      setVirtualDomainGroup(CDomainGroup::create(getId() + "_virtual_domain_group"));
      setVirtualAxisGroup(CAxisGroup::create(getId() + "_virtual_axis_group"));
@@ -54,6 +55,7 @@ namespace xios {
       , axisPositionInGrid_(), hasDomainAxisBaseRef_(false)
       , gridSrc_(), hasTransform_(false), isGenerated_(false), order_(), globalIndexOnServer_()
       , computedWrittenIndex_(false)
+      , clients()
    {
      setVirtualDomainGroup(CDomainGroup::create(getId() + "_virtual_domain_group"));
      setVirtualAxisGroup(CAxisGroup::create(getId() + "_virtual_axis_group"));
@@ -642,16 +644,15 @@ namespace xios {
           ++nbWritten;                
         } 
       }
-
    }
 
    //---------------------------------------------------------------
 
    /*
-     Compute the global index and its local index on taking account of mask, data index.
-     These global index then will be used to compute the connection of this client to other clients in the different group
+     Compute the global index and its local index taking account mask and data index.
+     These global indexes will be used to compute the connection of this client (sender) to its servers (receivers)
      (via function computeConnectedClient)
-     These global index also corresponding to data sent to other clients (if any)
+     These global indexes also correspond to data sent to servers (if any)
    */
    void CGrid::computeClientIndex()
    {
@@ -1346,12 +1347,12 @@ namespace xios {
   void CGrid::sendIndexScalarGrid()
   {
     CContext* context = CContext::getCurrent();
-    int nbSrvPools = (context->hasServer) ? (context->hasClient ? context->clientPrimServer.size() : 0) : 1;
     storeIndex_toSrv.clear();
+    std::set<CContextClient*>::iterator it;
 
-    for (int p = 0; p < nbSrvPools; ++p)
+    for (it=clients.begin(); it!=clients.end(); ++it)
     {
-      CContextClient* client = context->hasServer ? context->clientPrimServer[p] : context->client;
+      CContextClient* client = *it;
       int receiverSize = client->serverSize;
 
       CEventClient event(getType(), EVENT_ID_INDEX);
@@ -1411,11 +1412,12 @@ namespace xios {
   void CGrid::sendIndex(void)
   {
     CContext* context = CContext::getCurrent();
-    int nbSrvPools = (context->clientPrimServer.size() == 0) ? 1 : context->clientPrimServer.size();
     storeIndex_toSrv.clear();
-    for (int p = 0; p < nbSrvPools; ++p)
+    std::set<CContextClient*>::iterator it;
+
+    for (it=clients.begin(); it!=clients.end(); ++it)
     {
-      CContextClient* client = (context->clientPrimServer.size() == 0) ? context->client : context->clientPrimServer[p];
+      CContextClient* client = *it;
       int receiverSize = client->serverSize;
 
       CEventClient event(getType(), EVENT_ID_INDEX);
@@ -2499,6 +2501,11 @@ namespace xios {
       sendAddScalar(sList[i]->getId());
       sList[i]->sendAllAttributesToServer();
     }
+  }
+
+  void CGrid::setContextClient(CContextClient* contextClient)
+  {
+    clients.insert(contextClient);
   }
 
   /*!

dev/XIOS_DEV_CMIP6/src/node/grid.hpp
@@ -161,6 +161,8 @@ namespace xios {
          void sendIndex(void);
          void sendIndexScalarGrid();
 
+         void setContextClient(CContextClient* contextClient);
+
          void computeDomConServer();
          std::map<int, int> getDomConServerSide();
          std::map<int, StdSize> getAttributesBufferSize(CContextClient* client);
@@ -214,23 +216,34 @@ namespace xios {
 
          map<int, CArray<int, 1> > storeIndex_fromSrv; // Support, for now, reading with level-1 server
 
+         map<int, CArray<size_t, 1> > outIndexFromClient;  // Deprecated
 
-         map<int, CArray<size_t, 1> > outIndexFromClient, compressedOutIndexFromClient, outGlobalIndexFromClient;
+         map<int, CArray<size_t, 1> > compressedOutIndexFromClient;
 
-         // A client receives global index from other clients (via recvIndex)
-         // then does mapping these index into local index of STORE_CLIENTINDEX
-         // In this way, store_clientIndex can be used as an input of a source filter
-         // Maybe we need a flag to determine whether a client wants to write. TODO
+/** Map storing received indexes. Key = sender rank, value = index array. */
+         map<int, CArray<size_t, 1> > outGlobalIndexFromClient;
+
+// Manh Ha's comment: " A client receives global index from other clients (via recvIndex)
+// then does mapping these index into local index of STORE_CLIENTINDEX
+// In this way, store_clientIndex can be used as an input of a source filter
+// Maybe we need a flag to determine whether a client wants to write. TODO "
+
+/** Map storing received data. Key = sender rank, value = data array.
+ *  The map is created in CGrid::computeClientIndex and filled upon receiving data in CField::recvUpdateData() */
          map<int, CArray<size_t, 1> > outLocalIndexStoreOnClient; 
 
-/** Indexes calculated based on server distribution (serverDistribution_). They are used for writing data into a file. */
+/** Indexes calculated based on server-like distribution.
+ *  They are used for writing/reading data and only calculated for server level that does the writing/reading.
+ *  Along with localIndexToWriteOnClient, these indexes are used to correctly place incoming data. */
          CArray<size_t,1> localIndexToWriteOnServer;
 
-/** Indexes calculated based on client distribution (clientDistribution_). They are not used at all.
-    They should be the same as localIndexToWriteOnServer and potentially can be used as an additional check.*/
+/** Indexes calculated based on client-like distribution.
+ *  They are used for writing/reading data and only calculated for server level that does the writing/reading.
+ *  Along with localIndexToWriteOnServer, these indexes are used to correctly place incoming data. */
          CArray<size_t,1> localIndexToWriteOnClient;
 
          CArray<size_t,1> indexFromClients;
+
          void checkMask(void);
          void createMask(void);
          void modifyMask(const CArray<int,1>& indexToModify, bool valueToModify = false);
@@ -288,9 +301,13 @@ namespace xios {
         void computeConnectedClientsScalarGrid(); 
 
       private:
-         bool isChecked;
-         bool isDomainAxisChecked;
-         bool isIndexSent;
+
+/** Clients that have to send a grid. There can be multiple clients in case of secondary server, otherwise only one client. */
+        std::set<CContextClient*> clients;
+
+        bool isChecked;
+        bool isDomainAxisChecked;
+        bool isIndexSent;
 
         CDomainGroup* vDomainGroup_;
         CAxisGroup* vAxisGroup_;
@@ -298,10 +315,10 @@ namespace xios {
         std::vector<std::string> axisList_, domList_, scalarList_;
         bool isAxisListSet, isDomListSet, isScalarListSet;
 
-/** Distribution calculated in computeClientIndex() based on the knowledge of the entire grid */
+/** Client-like distribution calculated based on the knowledge of the entire grid */
         CDistributionClient* clientDistribution_;
 
-/** Distribution calculated upon receiving indexes */
+/** Server-like distribution calculated upon receiving indexes */
         CDistributionServer* serverDistribution_;
 
         CClientServerMapping* clientServerMap_;
@@ -344,24 +361,28 @@ namespace xios {
    template <int n>
    void CGrid::inputField(const CArray<double,n>& field, CArray<double,1>& stored) const
    {
+//#ifdef __XIOS_DEBUG
       if (this->getDataSize() != field.numElements())
          ERROR("void CGrid::inputField(const  CArray<double,n>& field, CArray<double,1>& stored) const",
                 << "[ Awaiting data of size = " << this->getDataSize() << ", "
                 << "Received data size = "      << field.numElements() << " ] "
                 << "The data array does not have the right size! "
                 << "Grid = " << this->GetName())
+//#endif
       this->storeField_arr(field.dataFirst(), stored);
    }
 
    template <int n>
    void CGrid::outputField(const CArray<double,1>& stored, CArray<double,n>& field) const
    {
+//#ifdef __XIOS_DEBUG
       if (this->getDataSize() != field.numElements())
          ERROR("void CGrid::outputField(const CArray<double,1>& stored, CArray<double,n>& field) const",
                 << "[ Size of the data = " << this->getDataSize() << ", "
                 << "Output data size = "   << field.numElements() << " ] "
                 << "The ouput array does not have the right size! "
                 << "Grid = " << this->GetName())
+//#endif
       this->restoreField_arr(stored, field.dataFirst());
    }
 

commit 2591082d24ea8412a26ae7ba0e79c84453b90fc5

Fixing a bug in case of reading introduced in r1294.
git-svn-id: svn+ssh://forge.ipsl.jussieu.fr/ipsl/forge/projets/ioserver/svn/XIOS@1320 d613d254-d18a-4304-b296-5125d4c4d670

dev/XIOS_DEV_CMIP6/src/node/field.cpp
@@ -1549,7 +1549,8 @@ namespace xios{
    void CField::setContextClient(CContextClient* contextClient)
    {
      client = contextClient;
-     grid->setContextClient(contextClient);
+     if (file->mode.isEmpty() || (!file->mode.isEmpty() && file->mode == CFile::mode_attr::write))
+       grid->setContextClient(contextClient);
    }
 
    CContextClient* CField::getContextClient()